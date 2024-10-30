b8a68f44df89342163b2717150f58b03b168f0a2
Brown <73099195+SraBrown@users.noreply.github.com>
2024-10-30 15:06:51 +0100

    Add files via upload

---
+++ assda/src/test/java/com/example/assda/AssdaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.assda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssdaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
